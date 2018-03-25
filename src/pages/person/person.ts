fdbf94d4e8d801b555fa218c387e31b63c1995f2
= <=>
2018-03-25 15:00:52 +0100

    sets weight measuerments

--- src/pages/person/person.ts
+++ src/pages/person/person.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+
+import { Chart } from "chart.js";
 
 /**
  * Generated class for the PersonPage page.
@@ -9,16 +11,109 @@ import { NavController, NavParams } from 'ionic-angular';
  */
 
 @Component({
-  selector: 'page-person',
-  templateUrl: 'person.html',
+  selector: "page-person",
+  templateUrl: "person.html"
 })
 export class PersonPage {
+  @ViewChild("lineCanvas") lineCanvas;
+  @ViewChild("lineCanvas1") lineCanvas1;
+  @ViewChild("doughnutCanvas") doughnutCanvas;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  lineChart: any;
+  lineChart1: any;
+  doughnutChart: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad PersonPage');
-  }
+    console.log("ionViewDidLoad PersonPage");
 
+
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: "line",
+      data: {
+        datasets: [
+          {
+            backgroundColor: ["rgba(34, 195, 204, 0.125)"],
+            borderColor: ["#22c3cc"],
+            borderWidth: 1,
+            data: [100, 20, 150, 30, 0]
+          }
+        ]
+      },
+      options: {
+        layout: {
+          padding: {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0
+          }
+        },
+        legend: { display: false },
+        title: {
+          display: true,
+          text: "last weight 4 weaks ago",
+          fontSize: 8,
+          color: "#bababa"
+        },
+        scales: {
+          yAxes: [
+            {
+              display: false
+            }
+          ]
+        }
+      }
+    });
+
+    this.lineChart1 = new Chart(this.lineCanvas1.nativeElement, {
+      type: "line",
+      data: {
+        datasets: [
+          {
+            backgroundColor: ["rgba(34, 195, 204, 0.125)"],
+            borderColor: ["#22c3cc"],
+            borderWidth: 1,
+            data: [100, 20, 150, 30, 0]
+          }
+        ]
+      },
+      options: {
+        layout: {
+          padding: {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0
+          }
+        },
+        legend: { display: false },
+        title: {
+          display: false,
+        },
+        scales: {
+          yAxes: [
+            {
+              display: false
+            }
+          ]
+        }
+      }
+    });
+
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: "doughnut",
+      data: {
+        labels: ["Fats", "Minirals", "Water", "Protein"],
+        datasets: [
+          {
+            data: [50, 4, 20, 10],
+            backgroundColor: ["#22c3cc", "#22c3cc", "#65a4ed", "#50e3c2"],
+            hoverBackgroundColor: ["#22c3cc", "#22c3cc", "#65a4ed", "#50e3c2"]
+          }
+        ]
+      }
+    });
+  }
 }
