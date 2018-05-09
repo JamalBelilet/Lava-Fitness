fdfd2a7d52fe98cceda1fc9db4cc5fab6a344b56
= <=>
2018-05-10 00:57:58 +0100

    updates proxy

--- src/providers/authentication/authentication.ts
+++ src/providers/authentication/authentication.ts
@@ -15,7 +15,7 @@ export class AuthenticationProvider {
   public config = {
     lang : "rtl",
 
-    baseUrl: "/api",
+    baseUrl: "http://lava.sa/api",
 
     AuthorizationKey: "as@dL8]Rn3$2S!anR",
     headers: new HttpHeaders({
